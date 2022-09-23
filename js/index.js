c12eeb931f766e9b04b79b676e4054287dbdc212
Renanppcosta <renanppcosta@gmail.com>
2022-09-22 23:33:17 -0300

    Adicionando eventos na página

---
+++ js/index.js
@@ -0,0 +1,34 @@
+let contador = 1;
+let imagesBanner = ["./images/Rectangle1.svg",
+"./images/Rectangle2.svg",
+"./images/Rectangle3.svg",
+"./images/Rectangle4.svg",
+"./images/Rectangle5.svg",
+"./images/Rectangle6.svg"
+]
+
+setInterval(()=>{
+    let banner = document.querySelector(".banner img");
+    banner.innerHTML = imagesBanner[contador];
+
+
+},5000)
+
+
+const menu = document.querySelector(".hamburguer-menu");
+const sidebar = document.getElementById("sidebar-menu");
+const textMessage = document.querySelector(".contact-us textarea")
+const textChars = document.getElementById("chars-left")
+
+menu.addEventListener("click", ()=>{
+    sidebar.classList.toggle("sidebar-visible");
+});
+
+
+textMessage.addEventListener("input", (event)=>{
+    const charsLeft = 100 - event.target.value.length;
+    textChars.innerText = `At least ${charsLeft} characters left`
+    if(event.target.value.length > 100){
+        textChars.innerText = ""
+    }
+})
